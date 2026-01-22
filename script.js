42bf64f8d18b18c169d362537a5f48c56ab93f7c
AyushDassan <ayushdassan@gmail.com>
2026-01-22 11:45:14 +0530

    Add JavaScript connection test and button alert

---
+++ script.js
@@ -0,0 +1,9 @@
+// Test if JS is connected
+console.log("JavaScript connected!");
+
+// Button click
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  alert("Welcome to my Flex Website!");
+});
